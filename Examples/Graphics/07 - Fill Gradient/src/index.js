a43ccd187c712b239c9ba086e7771fce29a445df
daddyjanno <jn.drugmand@gmail.com>
2024-10-11 17:50:59 +0200

    index

---
+++ Examples/Graphics/07 - Fill Gradient/src/index.js
@@ -0,0 +1,49 @@
+import { Application, FillGradient, Graphics } from 'pixi.js'
+
+const app = new Application()
+await app.init({
+    background: '#1099bb',
+    resizeTo: window,
+})
+document.body.appendChild(app.canvas)
+
+// Create a color array for the linear gradient
+const colorStops = [0xffffff, 0xff0000, 0x00ff00, 0x0000ff, 0x000000]
+
+// Create a fill gradient
+const gradientFill = new FillGradient(0, 0, 150, 150)
+
+// Add the color stops to the fill gradient
+colorStops.forEach((number, index) => {
+    const ratio = index / colorStops.length
+    gradientFill.addColorStop(ratio, number)
+})
+
+// Create a fill graphic
+const graphic1 = new Graphics().roundRect(0, 0, 150, 150, 50).fill(gradientFill)
+
+// Create a stroke graphic
+const graphic2 = new Graphics()
+    .roundRect(0, 0, 150, 150, 50)
+    .stroke({ width: 20, fill: gradientFill })
+
+graphic1.pivot.set(75, 75)
+graphic1.x = 150
+graphic1.y = 200
+
+graphic2.x = 350
+graphic2.y = 125
+
+let tick = 0
+
+app.ticker.add(() => {
+    tick += 0.025
+    graphic1.rotation += 0.01
+    graphic2
+        .clear()
+        .roundRect(0, 0, 150, 150, 50)
+        .stroke({ width: Math.sin(tick) * 100, fill: gradientFill })
+})
+
+app.stage.addChild(graphic1)
+app.stage.addChild(graphic2)
