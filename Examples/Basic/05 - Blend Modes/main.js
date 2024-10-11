9cfb975d4b6ee3e3d6739a944f988daf5c7072a6
daddyjanno <jn.drugmand@gmail.com>
2024-10-11 11:01:33 +0200

    reset blend modes

--- Examples/Basic/05 - Blend Modes/main.js
+++
@@ -1,7 +0,0 @@
-console.log('05 - Blending modes')
-
-import { Application } from '../../../node_modules/pixi.js/dist/pixi.mjs'
-
-const app = new Application()
-await app.init()
-document.body.appendChild(app.canvas)
