b63216f11eee40e76f3b4dd046b636546f594ac1
daddyjanno <jn.drugmand@gmail.com>
2024-10-14 11:17:03 +0200

    Interaction

--- Guide/07 - Interaction/main.js
+++ Guide/07 - Interaction/main.js
@@ -6,8 +6,45 @@ import {
 
 console.log('07 - Interaction')
 
+// const app = new Application()
+// await app.init({
+//     backgroundColor: 'brown',
+//     resizeTo: window,
+//     antialias: true,
+// })
+// document.body.appendChild(app.canvas)
+
+// await Assets.load('sample.png')
+
+// let sprite = Sprite.from('sample.png')
+
+//-----------------------
+// Enabling Interaction
+//-----------------------
+// sprite.on('pointerdown', (event) => {
+//     sprite.x = Math.random() * app.stage.width
+//     sprite.y = Math.random() * app.stage.height
+// })
+// sprite.eventMode = 'static'
+
+// console.log(sprite.isInteractive())
+
+// app.stage.addChild(sprite)
+
+//-----------------------
+// Optimization
+//-----------------------
+
 const app = new Application()
 await app.init({
+    eventMode: 'passive',
+    eventFeatures: {
+        move: true,
+        /** disables the global move events which can be very expensive in large scenes */
+        globalMove: false,
+        click: true,
+        wheel: true,
+    },
     backgroundColor: 'brown',
     resizeTo: window,
     antialias: true,
@@ -17,12 +54,12 @@ document.body.appendChild(app.canvas)
 await Assets.load('sample.png')
 
 let sprite = Sprite.from('sample.png')
-sprite.on('pointerdown', (event) => {
-    sprite.x = Math.random() * app.stage.width
-    sprite.y = Math.random() * app.stage.height
-})
-sprite.eventMode = 'static'
+app.stage.addChild(sprite)
 
 console.log(sprite.isInteractive())
-
-app.stage.addChild(sprite)
+sprite.on('pointerdown', (event) => {
+    sprite.x = (Math.random() * app.screen.width) / 2
+    sprite.y = (Math.random() * app.screen.height) / 2
+})
+sprite.eventMode = 'static'
+console.log(sprite.isInteractive())
