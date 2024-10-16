f7a9b95a4140a16f57fae1054f1e5c308fd47853
daddyjanno <jn.drugmand@gmail.com>
2024-10-16 10:53:22 +0200

    index.js

--- Examples/Events/08 - Slider/src/index.js
+++ Examples/Events/08 - Slider/src/index.js
@@ -1,32 +1,11 @@
-import { Application, Graphics } from 'pixi.js'
+import { Application } from 'pixi.js'
 
-console.log('Example - 07 - Pointer Tracker')
+console.log('Example - 08 - Slider')
 
 const app = new Application()
 await app.init({
     background: '#1099bb',
-    resizeTo: window,
+    resizeto: window,
     antialias: true,
 })
 document.body.appendChild(app.canvas)
-
-const circle = new Graphics()
-    .circle(0, 0, 8)
-    .fill({ color: 0xffffff })
-    .stroke({ color: 0x111111, alpha: 0.87, width: 1 })
-
-circle.position.set(app.screen.width / 2, app.screen.height / 2)
-
-app.stage.addChild(circle)
-
-// Enable interactivity!
-app.stage.eventMode = 'static'
-
-// Make sure the whole canvas area is interactive, not just the circle.
-app.stage.hitArea = app.screen
-
-// Follow the pointer
-
-app.stage.addEventListener('pointermove', (e) => {
-    circle.position.copyFrom(e.global)
-})
