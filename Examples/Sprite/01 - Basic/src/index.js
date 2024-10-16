febf825345bb7be0277d4ed70a3dd1628e843851
daddyjanno <jn.drugmand@gmail.com>
2024-10-16 15:57:12 +0200

    Basic

--- Examples/Sprite/01 - Basic/src/index.js
+++ Examples/Sprite/01 - Basic/src/index.js
@@ -1,1 +1,23 @@
+import { Application, Assets, Sprite } from 'pixi.js'
+
 console.log('Sprite - 01 - Basic')
+
+const app = new Application()
+await app.init({
+    background: '#1099bb',
+    resizeTo: window,
+    antialias: true,
+})
+document.body.appendChild(app.canvas)
+
+const texture = await Assets.load('https://pixijs.com/assets/bunny.png')
+
+const bunny = Sprite.from(texture)
+bunny.anchor.set(0.5)
+bunny.x = app.screen.width / 2
+bunny.y = app.screen.height / 2
+app.stage.addChild(bunny)
+
+app.ticker.add((time) => {
+    bunny.rotation += 0.1 * time.deltaTime
+})
