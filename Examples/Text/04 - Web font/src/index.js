badba0fc131cf50c68d410f392c847eafffb2ad1
daddyjanno <jn.drugmand@gmail.com>
2024-10-18 10:05:36 +0200

    web font setup

--- Examples/Text/04 - Web font/src/index.js
+++ Examples/Text/04 - Web font/src/index.js
@@ -1,1 +1,10 @@
+import { Application } from 'pixi.js'
+
 console.log('Example - 04 - Web font')
+
+const app = new Application()
+await app.init({
+    background: '#1099bb',
+    resizeTo: window,
+})
+document.body.appendChild(app.canvas)
