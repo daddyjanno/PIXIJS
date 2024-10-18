6f76a58e159a6985fd26e3a04d769a19fefcfbc5
daddyjanno <jn.drugmand@gmail.com>
2024-10-18 12:22:40 +0200

    Guide - textures

--- Guide/11 - Textures/main.js
+++ Guide/11 - Textures/main.js
@@ -1,7 +1,31 @@
 import {
     Application,
     Assets,
-    Text,
+    Sprite,
 } from '../../node_modules/pixi.js/dist/pixi.min.mjs'
 
 console.log('11 - Textures')
+
+const app = new Application()
+await app.init({
+    background: '#1099bb',
+    resizeTo: window,
+})
+document.body.appendChild(app.canvas)
+
+const texture = await Assets.load('https://pixijs.com/assets/flowerTop.png')
+
+// pass a texture explicitly
+const sprite = new Sprite(texture)
+
+//as options
+const sprite2 = new Sprite({ texture })
+sprite2.y = 200
+
+// from the cache as the texture is loaded
+const sprite3 = Sprite.from('https://pixijs.com/assets/flowerTop.png')
+sprite3.y = 400
+
+app.stage.addChild(sprite)
+app.stage.addChild(sprite2)
+app.stage.addChild(sprite3)
