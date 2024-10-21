0f548350b52689e5439400ef783efc2f4cd7aed3
daddyjanno <jn.drugmand@gmail.com>
2024-10-21 16:24:43 +0200

    index

--- Tutorials/Tutorial - 02/src/index.js
+++ Tutorials/Tutorial - 02/src/index.js
@@ -1,10 +1,76 @@
-import { Application } from 'pixi.js'
+import { Application, Assets } from 'pixi.js'
+import { addBackground } from './addBackground'
+import { addFishes, animateFish } from './addFishes'
+import { addWaterOverlay, animateWaterOverlay } from './addWaterOverlay'
+import { addDisplacementEffect } from './addDisplacementEffect'
 
 console.log('Tutorial 02')
 
 const app = new Application()
-await app.init({
-    background: '#1099bb',
-    resizeTo: window,
-})
-document.body.appendChild(app.canvas)
+
+const fishes = []
+
+async function setup() {
+    await app.init({
+        background: '#1099bb',
+        resizeTo: window,
+    })
+    document.body.appendChild(app.canvas)
+}
+
+async function preload() {
+    const assets = [
+        {
+            alias: 'background',
+            src: 'https://pixijs.com/assets/tutorials/fish-pond/pond_background.jpg',
+        },
+        {
+            alias: 'fish1',
+            src: 'https://pixijs.com/assets/tutorials/fish-pond/fish1.png',
+        },
+        {
+            alias: 'fish2',
+            src: 'https://pixijs.com/assets/tutorials/fish-pond/fish2.png',
+        },
+        {
+            alias: 'fish3',
+            src: 'https://pixijs.com/assets/tutorials/fish-pond/fish3.png',
+        },
+        {
+            alias: 'fish4',
+            src: 'https://pixijs.com/assets/tutorials/fish-pond/fish4.png',
+        },
+        {
+            alias: 'fish5',
+            src: 'https://pixijs.com/assets/tutorials/fish-pond/fish5.png',
+        },
+        {
+            alias: 'overlay',
+            src: 'https://pixijs.com/assets/tutorials/fish-pond/wave_overlay.png',
+        },
+        {
+            alias: 'displacement',
+            src: 'https://pixijs.com/assets/tutorials/fish-pond/displacement_map.png',
+        },
+    ]
+    await Assets.load(assets)
+}
+
+async function start() {
+    await setup()
+    await preload()
+    addBackground(app)
+    addFishes(app, fishes)
+
+    app.ticker.add((time) => {
+        animateFish(app, fishes, time)
+    })
+
+    addWaterOverlay(app)
+    app.ticker.add((time) => {
+        animateWaterOverlay(app, time)
+    })
+
+    addDisplacementEffect(app)
+}
+start()
