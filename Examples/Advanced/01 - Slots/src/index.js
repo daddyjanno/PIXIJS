d0224de3d2f36a7d4f14467e0c49a89736faef58
daddyjanno <jn.drugmand@gmail.com>
2024-10-21 12:30:39 +0200

    slots

--- Examples/Advanced/01 - Slots/src/index.js
+++ Examples/Advanced/01 - Slots/src/index.js
@@ -1,27 +1,29 @@
+console.log('Example - Advanced - 01 - Slots')
+
 import {
     Application,
     Assets,
-    BlurFilter,
     Color,
     Container,
-    FillGradient,
-    Graphics,
+    Texture,
     Sprite,
+    Graphics,
     Text,
     TextStyle,
-    Texture,
+    BlurFilter,
+    FillGradient,
 } from 'pixi.js'
 
-console.log('Example - Advanced - 01 - Slots')
-
+// Create a new application
 const app = new Application()
-await app.init({
-    background: '1099bb',
-    resizeTo: window,
-})
+
+// Initialize the application
+await app.init({ background: '#1099bb', resizeTo: window })
+
+// Append the application canvas to the document body
 document.body.appendChild(app.canvas)
 
-// Charger les textures:
+// Load the textures
 await Assets.load([
     'https://pixijs.com/assets/eggHead.png',
     'https://pixijs.com/assets/flowerTop.png',
@@ -29,82 +31,85 @@ await Assets.load([
     'https://pixijs.com/assets/skully.png',
 ])
 
-const REEL_WIDTH = 160 // Largeur de chaque colonne de rouleau
-const SYMBOL_SIZE = 150 // Taille de chaque symbole affiché
+const REEL_WIDTH = 160
+const SYMBOL_SIZE = 150
 
-// Créer différents symboles pour chaque rouleau
-const slotsTextures = [
+// Create different slot symbols
+const slotTextures = [
     Texture.from('https://pixijs.com/assets/eggHead.png'),
     Texture.from('https://pixijs.com/assets/flowerTop.png'),
     Texture.from('https://pixijs.com/assets/helmlok.png'),
     Texture.from('https://pixijs.com/assets/skully.png'),
 ]
 
-// Construire les rouleaux
-const reels = [] // Tableau pour stocker les rouleaux
-const reelsContainer = new Container() // Conteneur pour tous les rouleaux
+// Build the reels
+const reels = []
+const reelContainer = new Container()
 
 for (let i = 0; i < 5; i++) {
-    const rc = new Container() // Crée un conteneur pour un rouleau spécifique
-    rc.x = i * REEL_WIDTH // Positionne le container en fonction de l'index
-    reelsContainer.addChild(rc) // Ajoute ce container au container  principal
+    const rc = new Container()
+
+    rc.x = i * REEL_WIDTH
+    reelContainer.addChild(rc)
 
     const reel = {
-        container: rc, // Le conteneur de ce rouleau
-        symbols: [], // Les symboles du rouleau
-        position: 0, // Position initiale du rouleau
-        previousPosition: 0, // Position précédente pour calculer les mouvements
-        blur: new BlurFilter(), // Filtre de flou pour simuler la vitesse du rouleau
+        container: rc,
+        symbols: [],
+        position: 0,
+        previousPosition: 0,
+        blur: new BlurFilter(),
     }
+
     reel.blur.blurX = 0
     reel.blur.blurY = 0
-    rc.filters = [reel.blur] // Applique le filtre de flou au conteneur du rouleau
+    rc.filters = [reel.blur]
 
-    // Crée les symboles dans chaque rouleau
+    // Build the symbols
     for (let j = 0; j < 4; j++) {
         const symbol = new Sprite(
-            slotsTextures[Math.floor(Math.random() * slotsTextures.length)]
+            slotTextures[Math.floor(Math.random() * slotTextures.length)]
         )
+        // Scale the symbol to fit symbol area.
 
-        // Définit la position du symbole et son échelle pour l'ajuster à la taille définie
-        symbol.x = Math.round((SYMBOL_SIZE - symbol.width) / 2)
         symbol.y = j * SYMBOL_SIZE
         symbol.scale.x = symbol.scale.y = Math.min(
             SYMBOL_SIZE / symbol.width,
             SYMBOL_SIZE / symbol.height
         )
-
-        reel.symbols.push(symbol) // Ajoute le symbole à ce rouleau
-        rc.addChild(symbol) // Ajoute le symbole au conteneur du rouleau
+        symbol.x = Math.round((SYMBOL_SIZE - symbol.width) / 2)
+        reel.symbols.push(symbol)
+        rc.addChild(symbol)
     }
-    reels.push(reel) // Ajoute ce rouleau au tableau des rouleaux
+    reels.push(reel)
 }
-app.stage.addChild(reelsContainer) // Ajoute le conteneur de tous les rouleaux à la scène de l'application
+app.stage.addChild(reelContainer)
 
 // Build top & bottom covers and position reelContainer
-const margin = (app.screen.height - SYMBOL_SIZE * 3) / 2 // Calcule la marge en haut et en bas des rouleaux
-reelsContainer.x = Math.round(app.screen.width - REEL_WIDTH * 5)
-reelsContainer.y = margin
+const margin = (app.screen.height - SYMBOL_SIZE * 3) / 2
+
+reelContainer.y = margin
+reelContainer.x = Math.round(app.screen.width - REEL_WIDTH * 5)
 const top = new Graphics()
     .rect(0, 0, app.screen.width, margin)
-    .fill({ color: 0x0 }) // Rectangle noir en haut
+    .fill({ color: 0x0 })
 const bottom = new Graphics()
     .rect(0, SYMBOL_SIZE * 3 + margin, app.screen.width, margin)
-    .fill({ color: 0x0 }) // Rectangle noir en bas
+    .fill({ color: 0x0 })
 
-// Création du texte d'en-tête et de jeu
-// Un dégradé est créé pour être utilisé comme remplissage du texte
-const fill = new FillGradient(0, 0, 0, 36 * 1.7) // Crée un dégradé de remplissage
+// Create gradient fill
+const fill = new FillGradient(0, 0, 0, 36 * 1.7)
 
 const colors = [0xffffff, 0x00ff99].map((color) =>
     Color.shared.setValue(color).toNumber()
 )
+
 colors.forEach((number, index) => {
     const ratio = index / colors.length
+
     fill.addColorStop(ratio, number)
 })
 
-// Ajout du texte "Spin the wheels!" et "PIXI MONSTER SLOTS!"
+// Add play text
 const style = new TextStyle({
     fontFamily: 'Arial',
     fontSize: 36,
@@ -122,16 +127,149 @@ const style = new TextStyle({
     wordWrapWidth: 440,
 })
 
-const playText = new Text({ text: 'Tournez les roues', style })
+const playText = new Text('Spin the wheels!', style)
+
 playText.x = Math.round((bottom.width - playText.width) / 2)
 playText.y =
     app.screen.height - margin + Math.round((margin - playText.height) / 2)
 bottom.addChild(playText)
 
-const headerText = new Text({ text: 'PIXI MONSTER SLOTS!', style })
+// Add header text
+const headerText = new Text('PIXI MONSTER SLOTS!', style)
+
 headerText.x = Math.round((top.width - headerText.width) / 2)
 headerText.y = Math.round((margin - headerText.height) / 2)
 top.addChild(headerText)
 
 app.stage.addChild(top)
 app.stage.addChild(bottom)
+
+// Set the interactivity.
+bottom.eventMode = 'static'
+bottom.cursor = 'pointer'
+bottom.addListener('pointerdown', () => {
+    startPlay()
+})
+
+let running = false
+
+// Function to start playing.
+function startPlay() {
+    if (running) return
+    running = true
+
+    for (let i = 0; i < reels.length; i++) {
+        const r = reels[i]
+        const extra = Math.floor(Math.random() * 3)
+        const target = r.position + 10 + i * 5 + extra
+        const time = 2500 + i * 600 + extra * 600
+
+        tweenTo(
+            r,
+            'position',
+            target,
+            time,
+            backout(0.5),
+            null,
+            i === reels.length - 1 ? reelsComplete : null
+        )
+    }
+}
+
+// Reels done handler.
+function reelsComplete() {
+    running = false
+}
+
+// Listen for animate update.
+app.ticker.add(() => {
+    // Update the slots.
+    for (let i = 0; i < reels.length; i++) {
+        const r = reels[i]
+        // Update blur filter y amount based on speed.
+        // This would be better if calculated with time in mind also. Now blur depends on frame rate.
+
+        r.blur.blurY = (r.position - r.previousPosition) * 8
+        r.previousPosition = r.position
+
+        // Update symbol positions on reel.
+        for (let j = 0; j < r.symbols.length; j++) {
+            const s = r.symbols[j]
+            const prevy = s.y
+
+            s.y =
+                ((r.position + j) % r.symbols.length) * SYMBOL_SIZE -
+                SYMBOL_SIZE
+            if (s.y < 0 && prevy > SYMBOL_SIZE) {
+                // Detect going over and swap a texture.
+                // This should in proper product be determined from some logical reel.
+                s.texture =
+                    slotTextures[
+                        Math.floor(Math.random() * slotTextures.length)
+                    ]
+                s.scale.x = s.scale.y = Math.min(
+                    SYMBOL_SIZE / s.texture.width,
+                    SYMBOL_SIZE / s.texture.height
+                )
+                s.x = Math.round((SYMBOL_SIZE - s.width) / 2)
+            }
+        }
+    }
+})
+
+// Very simple tweening utility function. This should be replaced with a proper tweening library in a real product.
+const tweening = []
+
+function tweenTo(object, property, target, time, easing, onchange, oncomplete) {
+    const tween = {
+        object,
+        property,
+        propertyBeginValue: object[property],
+        target,
+        easing,
+        time,
+        change: onchange,
+        complete: oncomplete,
+        start: Date.now(),
+    }
+
+    tweening.push(tween)
+
+    return tween
+}
+// Listen for animate update.
+app.ticker.add(() => {
+    const now = Date.now()
+    const remove = []
+
+    for (let i = 0; i < tweening.length; i++) {
+        const t = tweening[i]
+        const phase = Math.min(1, (now - t.start) / t.time)
+
+        t.object[t.property] = lerp(
+            t.propertyBeginValue,
+            t.target,
+            t.easing(phase)
+        )
+        if (t.change) t.change(t)
+        if (phase === 1) {
+            t.object[t.property] = t.target
+            if (t.complete) t.complete(t)
+            remove.push(t)
+        }
+    }
+    for (let i = 0; i < remove.length; i++) {
+        tweening.splice(tweening.indexOf(remove[i]), 1)
+    }
+})
+
+// Basic lerp funtion.
+function lerp(a1, a2, t) {
+    return a1 * (1 - t) + a2 * t
+}
+
+// Backout function from tweenjs.
+// https://github.com/CreateJS/TweenJS/blob/master/src/tweenjs/Ease.js
+function backout(amount) {
+    return (t) => --t * t * ((amount + 1) * t + amount) + 1
+}
