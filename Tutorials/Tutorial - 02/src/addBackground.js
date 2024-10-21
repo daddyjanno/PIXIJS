db9a83fbf40e818cf1d5e8780ee829a1c887bc28
daddyjanno <jn.drugmand@gmail.com>
2024-10-21 16:12:11 +0200

    addBackground

---
+++ Tutorials/Tutorial - 02/src/addBackground.js
@@ -0,0 +1,30 @@
+import { Sprite } from 'pixi.js'
+
+export function addBackground(app) {
+    // Create a background sprite.
+    const background = Sprite.from('background')
+    // Center background sprite anchor.
+    background.anchor.set(0.5)
+
+    /**
+     * If the preview is landscape, fill the width of the screen
+     * and apply horizontal scale to the vertical scale for a uniform fit.
+     */
+    if (app.stage.width > app.screen.height) {
+        background.width = app.stage.width * 1.2
+        background.scale.y = background.scale.x
+    } else {
+        /**
+         * If the preview is square or portrait, then fill the height of the screen instead
+         * and apply the scaling to the horizontal scale accordingly.
+         */
+        background.height = app.screen.height * 1.2
+        background.scale.x = background.scale.y
+    }
+
+    // Position the background sprite in the center of the stage.
+    background.x = app.screen.width / 2
+    background.y = app.screen.height / 2
+
+    app.stage.addChild(background)
+}
