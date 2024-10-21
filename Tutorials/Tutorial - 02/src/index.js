ca34b506a1a005552289681a5e78b4d3fe0c1db4
daddyjanno <jn.drugmand@gmail.com>
2024-10-21 15:38:22 +0200

    app.init

--- Tutorials/Tutorial - 02/src/index.js
+++ Tutorials/Tutorial - 02/src/index.js
@@ -1,1 +1,10 @@
+import { Application } from 'pixi.js'
+
 console.log('Tutorial 02')
+
+const app = new Application()
+await app.init({
+    background: '#1099bb',
+    resizeTo: window,
+})
+document.body.appendChild(app.canvas)
