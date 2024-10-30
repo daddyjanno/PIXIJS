01d61b37c1998f730696bdced9fb267534a82419
daddyjanno <jn.drugmand@gmail.com>
2024-10-30 14:59:34 +0100

    index

--- Tutorials/Tutorial - 04 - Spine Boy/src/index.js
+++ Tutorials/Tutorial - 04 - Spine Boy/src/index.js
@@ -1,7 +1,6 @@
 import 'pixi-spine'
 import { Application, Assets } from 'pixi.js'
 import { SpineBoy } from './SpineBoy'
-import { Spine } from '@esotericsoftware/spine-pixi'
 
 console.log('Tutorial 04')
 
@@ -21,5 +20,16 @@ Assets.add({
     alias: 'spineAtlas',
     src: './assets/spineboy-pma.atlas',
 })
+
+// Create our character
+const spineBoy = new SpineBoy()
+
+// Adjust character transformation.
+spineBoy.view.x = app.screen.width / 2
+spineBoy.view.y = app.screen.height - 80
+spineBoy.spine.scale.set(0.5)
+
+// Add character to the stage.
+app.stage.addChild(spineBoy.view)
 
 await Assets.load(['spineSkeleton', 'spineAtlas'])
