16dfbc9bcea0b7c91b340e91de3be5ad5171d3bb
daddyjanno <jn.drugmand@gmail.com>
2024-10-30 12:48:30 +0100

    index

--- Tutorials/Tutorial - 04 - Spine Boy Adventure/src/index.js
+++ Tutorials/Tutorial - 04 - Spine Boy Adventure/src/index.js
@@ -1,1 +1,11 @@
+import { Application } from 'pixi.js'
+
 console.log('Tutorial 04')
+
+const app = new Application()
+await app.init({
+    background: '#1099bb',
+    resizeTo: window,
+    antialias: true,
+})
+document.body.appendChild(app.canvas)
